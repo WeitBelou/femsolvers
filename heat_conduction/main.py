43807db1275596479e894e7778a81111a1638255
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-06-02 00:48:13 +0300

    Add documentation to mesh and boundary generators.

--- heat_conduction/main.py
+++ heat_conduction/main.py
@@ -8,8 +8,13 @@ from dolfin.cpp.mesh import Point
 from fenics import *
 
 
-# Create cylinder mesh
-def create_mesh(r, h):
+def create_cylinder_mesh(r, h):
+    """
+    Create cylinder mesh with bottom in (0, 0, 0).
+    :param r: cylinder radius
+    :param h: cylinder height
+    :return: generated mesh
+    """
     return generate_mesh(
         Cylinder(
             top=Point(0, 0, h),
@@ -19,12 +24,18 @@ def create_mesh(r, h):
     )
 
 
-def create_boundary_conditions(V, h):
+def create_boundary_conditions(function_space, h):
+    """
+    Create boundary conditions: two planes with fixed temperature.
+    :param function_space: function space for which boundary conditions will be created.
+    :param h: distance between planes with fixed temperature.
+    :return: list of boundary conditions.
+    """
     u_top = Expression('0', degree=3)
     u_bottom = Expression('100', degree=3)
 
-    bottom_bc = DirichletBC(V, u_top, lambda x, on_boundary: on_boundary and near(x[2], h))
-    upper_bc = DirichletBC(V, u_bottom, lambda x, on_boundary: on_boundary and near(x[2], 0))
+    bottom_bc = DirichletBC(function_space, u_top, lambda x, on_boundary: on_boundary and near(x[2], h))
+    upper_bc = DirichletBC(function_space, u_bottom, lambda x, on_boundary: on_boundary and near(x[2], 0))
 
     return [upper_bc, bottom_bc]
 
@@ -32,11 +43,11 @@ def create_boundary_conditions(V, h):
 if __name__ == '__main__':
     R = 10
     H = 5
-    mesh = create_mesh(R, H)
+
+    mesh = create_cylinder_mesh(R, H)
 
     V = FunctionSpace(mesh, 'P', 1)
 
-    # Define BC
     bcs = create_boundary_conditions(V, H)
 
     # Define variational problem
