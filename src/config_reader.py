1d381d7134f5debb48f37b7d2e7a9be682012add
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-07-07 21:58:26 +0300

    Add output of config.

--- src/config_reader.py
+++ src/config_reader.py
@@ -11,14 +11,23 @@ class Config:
     Class that encapsulates configuration.
     """
 
-    def __init__(self, config):
-        self.material = config.material
-        self.geometry = config.geometry
-        self.drilling = config.drilling
+    def __init__(self, config: dict):
+        self._raw_data = config
+
+        self.material = config['material']
+        self.geometry = config['geometry']
+        self.drilling = config['drilling']
+
+    def __str__(self):
+        """
+        Return indented data in json format.
+        :return: pretty printed json representation of config.
+        """
+        return json.dumps(self._raw_data, indent=2)
 
 
 def read_config(config_file: str) -> Config:
     with open(config_file, 'r', encoding='utf-8') as f:
         config = Config(json.load(f))
-        logger.info('Config:\n%(config)s', {'config': json.dumps(obj=config, indent=2)})
+        logger.info('Config:\n%(config)s', {'config': config})
         return config
