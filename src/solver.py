8c556dc39072af44f3d090b18fea769baa09dbe1
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-07-08 00:55:59 +0300

    Add simple linear elasticity solver.

--- src/solver.py
+++ src/solver.py
@@ -1,9 +1,10 @@
+import fenics
 import mshr
+from dolfin.cpp.function import near
 from dolfin.cpp.io import File
 from dolfin.cpp.mesh import Point
-from fenics import Mesh
-
-from config_reader import Config, Geometry
+from fenics import *
+from config_reader import Config, Geometry, Material
 from logger import get_logger
 
 
@@ -21,8 +22,44 @@ def create_mesh(geometry: Geometry) -> Mesh:
     return mesh
 
 
+def epsilon(v: TestFunction):
+    return sym(nabla_grad(v))
+
+
+def sigma(u: TrialFunction, material: Material):
+    E = material.young_modulus
+    G = material.shear_modulus
+
+    lambda_ = G * (E - 2 * G) / (3 * G - E)
+    mu = G
+
+    return lambda_ * nabla_div(u) * Identity(3) + 2 * mu * epsilon(u)
+
+
 def solve(config: Config):
     mesh = create_mesh(config.geometry)
 
     vtkfile = File('results/mesh.pvd')
     vtkfile << mesh
+
+    V = VectorFunctionSpace(mesh, 'P', 1)
+
+    u = TrialFunction(V)
+    v = TestFunction(V)
+
+    gravity = Point(0, 0, -9.8)
+    f = Constant(config.material.density * gravity)
+    T = Constant((0, 0, 0))
+
+    a = inner(sigma(u, config.material), epsilon(v)) * dx
+    L = dot(f, v) * dx + dot(T, v) * ds
+
+    bc = DirichletBC(V, Constant((0, 0, 0)),
+                     lambda x, on_boundary: on_boundary and near(x[2], 0))
+
+    u = Function(V)
+
+    fenics.solve(a == L, u, bc)
+
+    vtkfile = File('results/displacement.pvd')
+    vtkfile << u
