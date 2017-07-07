ae72091ac75fccd68ed469b46bcc5ae9a7571a38
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-07-07 23:21:09 +0300

    Add mesh generation and output.

--- src/solver.py
+++ src/solver.py
@@ -1,5 +1,28 @@
-from config_reader import Config
+import mshr
+from dolfin.cpp.io import File
+from dolfin.cpp.mesh import Point
+from fenics import Mesh
+
+from config_reader import Config, Geometry
+from logger import get_logger
+
+
+def create_mesh(geometry: Geometry) -> Mesh:
+    radius = geometry.diameter / 2
+    full_height = geometry.full_height
+    mesh = mshr.generate_mesh(
+        mshr.Cylinder(top=Point(0.0, 0.0, full_height),
+                      bottom=Point(0.0, 0.0, 0.0),
+                      top_radius=radius,
+                      bottom_radius=radius),
+        128  # Resolution
+    )
+    get_logger(__name__).info('Created mesh: %(mesh)s', {'mesh': mesh})
+    return mesh
 
 
 def solve(config: Config):
-    pass
+    mesh = create_mesh(config.geometry)
+
+    vtkfile = File('results/mesh.pvd')
+    vtkfile << mesh
