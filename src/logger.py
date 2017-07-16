37ecb392cbeb09537e234438d2de83ca21d28dd9
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-07-16 19:40:03 +0300

    Set base logging level.
    Signed-off-by: Ivan Kosolapov <ivan.kosolapov@phystech.edu>

--- src/logger.py
+++ src/logger.py
@@ -5,7 +5,8 @@ def configure_logger():
     """
     Configure logger and suppress some fenics logs
     """
-    logging.basicConfig(format='[%(name)s] [%(levelname)s] %(asctime)s %(message)s')
+    logging.basicConfig(format='[%(name)s] [%(levelname)s] %(asctime)s %(message)s',
+                        level=logging.DEBUG)
 
     logging.getLogger('FFC').setLevel(logging.WARNING)
     logging.getLogger('UFL').setLevel(logging.WARNING)
