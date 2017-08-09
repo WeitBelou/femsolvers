3cf930e0370043f378334626acbeb65d58896107
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-08-09 17:03:11 +0300

    Add config parsing for mesh geometry.

--- femsolvers/main.py
+++ femsolvers/main.py
@@ -1,7 +1,6 @@
 import os
 
-from dolfin.cpp.mesh import UnitCubeMesh
-
+from meshes import create_mesh_from_geometry
 from utils import write_data
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
@@ -9,5 +8,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 if __name__ == '__main__':
     MESHES_DIR = os.path.join(BASE_DIR, 'meshes')
 
-    mesh = UnitCubeMesh(32, 32, 32)
+    mesh = create_mesh_from_geometry(dict(
+        type='cylinder', radius=150.0, height=10.0, bottom=[0.0, 0.0, 0.0]
+    ))
     write_data(MESHES_DIR, 'mesh.pvd', mesh)
