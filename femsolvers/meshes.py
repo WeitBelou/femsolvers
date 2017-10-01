86bcd847e36ee96999d65d3e646351b5c389e5e4
Ivan Kosolapov <ivan.kosolapov@phystech.edu>
2017-10-01 16:22:24 +0300

    Add logging and mesh generation.

--- femsolvers/meshes.py
+++ femsolvers/meshes.py
@@ -1,41 +1,23 @@
+from typing import Iterable
+
 import mshr
 from dolfin.cpp.mesh import Mesh, Point
 
 
-class UnknownGeometryType(Exception):
-    def __init__(self, type_):
-        self._type = type_
+class Cylinder:
+    def __init__(self, radius: float, height: float, bottom: Iterable = (0.0, 0.0, 0.0)):
+        """
+        Create cylinder along Z axis.
 
-    def __str__(self):
-        return 'Unknown geometry type: {type}'.format(type=self._type)
+        :param radius: radius of cylinder.
+        :param height: height of cylinder, top in the positive direction of z axis.
+        :param bottom: bottom point of cylinder.
+        """
+        top = Point(bottom) + Point(0.0, 0.0, height)
+        geometry = mshr.Cylinder(top=top, bottom=bottom, top_radius=radius, bottom_radius=radius)
 
+        self._mesh = mshr.generate_mesh(geometry, 15.0)
 
-def create_mesh_from_geometry(geometry: dict) -> Mesh:
-    """
-    Creates mesh from geometry description.
-
-    :raises: UnknownGeometryType if trying to pass geometry with unknown type
-    """
-    geometry_type = geometry['type']
-
-    if geometry_type == 'cylinder':
-        radius = geometry['radius']
-        height = geometry['height']
-        bottom = geometry['bottom']
-        return _create_cylinder_mesh(radius, height, Point(bottom[0], bottom[1], bottom[2]))
-    else:
-        raise UnknownGeometryType(geometry_type)
-
-
-def _create_cylinder_mesh(radius: float, height: float, bottom: Point = Point()) -> Mesh:
-    """
-    Create cylinder along Z axis.
-
-    :param radius: radius of cylinder.
-    :param height: height of cylinder, top in the positive direction of z axis.
-    :param bottom: bottom point of cylinder.
-    :return: mesh object
-    """
-    top = bottom + Point(0.0, 0.0, height)
-    geometry = mshr.Cylinder(top=top, bottom=bottom, top_radius=radius, bottom_radius=radius)
-    return mshr.generate_mesh(geometry, 15.0)
+    @property
+    def mesh(self) -> Mesh:
+        return self._mesh
